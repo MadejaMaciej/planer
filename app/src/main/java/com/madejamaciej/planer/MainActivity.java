1f046caf094ec543739591be5ae69cf8a6d915f3
MadejaMaciej <48794984+MadejaMaciej@users.noreply.github.com>
2022-01-18 18:16:23 +0100

    work on displaying tasks

--- app/src/main/java/com/madejamaciej/planer/MainActivity.java
+++ app/src/main/java/com/madejamaciej/planer/MainActivity.java
@@ -2,12 +2,36 @@ package com.madejamaciej.planer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.CalendarView;
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+    int mYear;
+    int mMonth;
+    int mDay;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        CalendarView calendarView = (CalendarView) findViewById(R.id.mainCalendar);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(calendarView.getDate());
+        mYear = calendar.get(Calendar.YEAR);
+        mMonth = calendar.get(Calendar.MONTH);
+        mDay = calendar.get(Calendar.DAY_OF_MONTH);
+        displayDateInfo(mYear, mMonth, mDay);
+    }
+
+    private void displayDateInfo(int year, int month, int day){
+
+    }
+
+    public void chooseDate(View view) {
+
     }
 }
